dd092f313f000aff3424bfc99d9fadea31db6f98
Wildan Belfiore <mikewildan@outlook.com>
2024-03-11 23:14:37 +0700

    penghapusan hak akses profil pada user selain admin

--- routes/web.php
+++ routes/web.php
@@ -31,12 +31,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
 });
 
 Route::prefix('superadmin')->middleware(['auth', 'checkRole:superadmin'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //User Configurator
     Route::get('/', [SuperAdminController::class, 'index'])->name('superadmin.index');
     Route::patch('/update-user-roles', [SuperAdminController::class, 'updateUserRoles'])->name('superadmin.updateUserRoles');
 
