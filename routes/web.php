abbb8569db6ebd8517ac7d9422fc7cca26331480
Wildan Belfiore <mikewildan@outlook.com>
2024-03-14 21:33:09 +0700

    penghapusan hak akses profil pada user selain admin

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::prefix('superadmin')->middleware(['auth', 'checkRole:superadmin'])->group(function () {
+
+    // Akses profil user
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
