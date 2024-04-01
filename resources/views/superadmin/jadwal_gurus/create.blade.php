af278977ef48559416ca820722c6f28f4f5607e6
Wildan Belfiore <mikewildan@outlook.com>
2024-04-01 13:53:16 +0700

    perbaikan dark mode agar menjadi lebih konsisten lagi

--- resources/views/superadmin/jadwal_gurus/create.blade.php
+++ resources/views/superadmin/jadwal_gurus/create.blade.php
@@ -29,7 +29,7 @@
                             <label for="mata_pelajaran_id" class="block text-sm font-medium text-gray-600 dark:text-gray-200">
                                 Mata Pelajaran
                             </label>
-                            <select name="mata_pelajaran_id" id="mata_pelajaran_id" class="mt-1 p-2 border rounded-md w-full" required>
+                            <select name="mata_pelajaran_id" id="mata_pelajaran_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                                 <option value="" disabled selected>Pilih Mata Pelajaran</option>
                                 @foreach($mapels as $mapel)
                                 <option value="{{ $mapel->id }}">{{ $mapel->nama_mapel }} - {{ $mapel->mapel_id }}</option>
@@ -42,7 +42,7 @@
                             <label for="kelas_id" class="block text-sm font-medium text-gray-600 dark:text-gray-200">
                                 Kelas
                             </label>
-                            <select name="kelas_id" id="kelas_id" class="mt-1 p-2 border rounded-md w-full" required>
+                            <select name="kelas_id" id="kelas_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                                 @foreach ($kelas as $kelas)
                                 <option value="{{ $kelas->id }}">{{ $kelas->nama_kelas }}</option>
                                 @endforeach
@@ -54,7 +54,7 @@
                             <label for="hari" class="block text-sm font-medium text-gray-600 dark:text-gray-200">
                                 Hari
                             </label>
-                            <input type="date" name="hari" id="hari" class="mt-1 p-2 border rounded-md w-full" required placeholder="Pilih Hari">
+                            <input type="date" name="hari" id="hari" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required placeholder="Pilih Hari">
                         </div>
 
                         <!-- Waktu -->
@@ -62,7 +62,7 @@
                             <label for="waktu" class="block text-sm font-medium text-gray-600 dark:text-gray-200">
                                 Waktu
                             </label>
-                            <input type="time" name="waktu" id="waktu" class="mt-1 p-2 border rounded-md w-full" required placeholder="Pilih Waktu">
+                            <input type="time" name="waktu" id="waktu" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required placeholder="Pilih Waktu">
                         </div>
                         <!-- Guru Pengampu -->
                         <div class="mb-4">
@@ -81,12 +81,12 @@
 
                     <!-- Tombol Submit -->
                     <div class="flex items-center justify-between mt-4">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Simpan
                         </button>
 
                         <!-- Tombol Batal -->
-                        <a href="{{ route('superadmin.jadwal_gurus.index') }}" class="text-gray-600 dark:text-gray-300 hover:underline">Batal</a>
+                        <a href="{{ route('superadmin.jadwal_gurus.index') }}" class="text-white inline-flex items-center bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Batal</a>
                     </div>
                 </form>
             </div>
@@ -94,64 +94,78 @@
     </div>
 
     <!-- Modal Guru Pengampu -->
-    <div class="fixed inset-0 z-50 overflow-auto bg-gray-500 bg-opacity-75" id="guruPengampuModal" style="display: none;">
-        <div class="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/4 bg-white rounded shadow-lg">
-            <div class="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
-                <h3 class="text-2xl font-semibold">Pilih Guru Pengampu</h3>
-                <button type="button" class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 text-2xl leading-none font-semibold outline-none focus:outline-none" onclick="closeModal()">
-                    <span class="bg-transparent text-black opacity-5 h-6 w-6 text-xl block outline-none focus:outline-none">×</span>
-                </button>
-            </div>
-            <div class="relative p-6 flex-auto max-h-60 overflow-auto">
-                <!-- Search Input -->
-                <input type="text" id="searchGuru" class="mb-4 p-2 border rounded-md w-full" placeholder="Cari guru...">
-
-                <!-- Table to display Guru Pengampu data -->
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    <div class="flex items-center">
-                                        Nama Guru
-                                        <a href="#">
-                                            <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087a1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985a2.074 2.074 0 0 0-1.846-1.087Z" />
-                                            </svg>
-                                        </a>
-                                    </div>
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    <div class="flex items-center">
-                                        Spesialis
-                                        <a href="#">
-                                            <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087a1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985a2.074 2.074 0 0 0-1.846-1.087Z" />
-                                            </svg>
-                                        </a>
-                                    </div>
-                                </th>
-                                <th scope="col" class="px-6 py-3">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody id="guruTable">
-                            @foreach($gurus as $guru)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $guru->name }}</td>
-                                <td class="px-6 py-4">{{ $guru->spesialis }}</td>
-                                <td class="px-6 py-4">
-                                    <button type="button" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onclick="selectGuruPengampu({{ $guru->id }},'{{ $guru->name }}')">
-                                        <i class="fas fa-check-circle"></i> Pilih
-                                    </button>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+    <div id="guruPengampuModal" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full" style="display: none;">
+        <div class="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/4 rounded shadow-lg">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Tambah Data</h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onclick="closeModal()">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
                 </div>
-            </div>
-            <div class="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
-                <button type="button" class="bg-gray-500 text-white px-4 py-2 rounded-md focus:outline-none focus:shadow-outline-gray active:bg-gray-800" onclick="closeModal()">Tutup</button>
+                <div class="relative p-6 flex-auto">
+                    <!-- Search Input -->
+                    <div class="mb-4">
+                        <input type="text" id="searchGuru" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Cari guru...">
+                    </div>
+                    <!-- Table to display Guru Pengampu data -->
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Nama Guru
+                                            <a href="#">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087a1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985a2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Spesialis
+                                            <a href="#">
+                                                <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087a1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985a2.074 2.074 0 0 0-1.846-1.087Z" />
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody id="guruTable">
+                                @foreach($gurus as $guru)
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $guru->name }}</td>
+                                    <td class="px-6 py-4">{{ $guru->spesialis }}</td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onclick="selectGuruPengampu({{ $guru->id }},'{{ $guru->name }}')">
+                                            <i class="fas fa-check-circle"></i> Pilih
+                                        </button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="flex items-center justify-between p-6 border-t border-solid rounded-b border-blueGray-200">
+                    <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                        </svg>
+                        Simpan
+                    </button>
+                    <button type="button" class="text-white inline-flex items-center bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800" onclick="closeModal()">
+                        Batal
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -192,5 +206,4 @@
             }
         });
     </script>
-
 </x-app-layout>
